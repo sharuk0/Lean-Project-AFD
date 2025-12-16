31f2a71db94b9f41ce3c51bbdbbf4e49d029b125
sharuko <sharuko.g@gmail.com>
2025-12-15 21:43:04 -0500

    basic DFA structure testing

--- Project-Code/ProjectCode/Basic.lean
+++ Project-Code/ProjectCode/Basic.lean
@@ -1,1 +1,13 @@
-def hello := "world"
+import Mathlib
+#check Prop
+#check Type
+#check Type 1
+
+#check ℕ → Type
+
+structure DFA (α : Type) :=
+  (Q : Type)
+  [finite_Q : Fintype Q]
+  (δ : Q → α → Q)
+  (q0 : Q)
+  (F : Set Q)
